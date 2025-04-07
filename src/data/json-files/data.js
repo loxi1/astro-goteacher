47be84f1ba768e02413cfa3c846149391f13429c
loxi1 <anibal.cayetano@gmail.com>
2025-04-07 02:27:15 -0500

    change, subir imagenes

--- src/data/json-files/data.js
+++ src/data/json-files/data.js
@@ -5,48 +5,48 @@ export const carrusel = {
 			"subtitulo": "Escolar",
 			"texto": "Reserva tu clase, Ahora!",
 			"descripcion":"Potencia tu aprendizaje fuera del aula. Una herramienta clave para consolidar y fortalecer lo aprendido, brindando apoyo adicional para alcanzar tus metas académicas.",
-			"image": "primer-slider.webp",
-			"image_2": "segundo-slider.webp",
+			"image": "escolar-clases.webp",
+			"image_2": "escolar-haciendo-tareas.webp",
 			"elemento1": "elemento-colores.svg",
 			"elemento2": "elemento-estrella.svg",
 			"elemento3": "elemento-cuadrado.svg",
 			"elemento4": "elemento-espina.svg",			
 			"categoria": "escolar",
-			"detalle_1": "escolar_1.jpeg",
-			"detalle_2": "escolar_2.jpeg",
-			"detalle_3": "escolar_3.jpeg"
+			"detalle_1": "escolar-1.webp",
+			"detalle_2": "escolar-2.webp",
+			"detalle_3": "escolar-3.webp"
 		},
 		{
 			"titulo": "Reforzamiento",
 			"subtitulo": "Universitario",
 			"texto": "Reserva tu clase, Ahora!",
 			"descripcion":"En cuanto a nivel universitario brindamos cursos generales como matemática financiera, matemática para ingenieros, física universitaria, química universitaria",
-			"image": "tercer-slider.webp",
-			"image_2": "cuarto-slider.webp",
+			"image": "universitario-estudiando.webp",
+			"image_2": "universitario-profesor.webp",
 			"elemento1": "elemento-elice.svg",
 			"elemento2": "elemento-plligono-1.svg",
 			"elemento3": "elemento-estrellado.svg",
 			"elemento4": "elemento-hexagonal.svg",
 			"categoria": "universitario",
-			"detalle_1": "escolar_1.jpeg",
-			"detalle_2": "escolar_2.jpeg",
-			"detalle_3": "escolar_3.jpeg"
+			"detalle_1": "universitario-1.webp",
+			"detalle_2": "universitario-2.webp",
+			"detalle_3": "universitario-3.webp"
 		},
 		{
 			"titulo": "Reforzamiento",
 			"subtitulo": "Bachillerato",
 			"texto": "Reserva tu clase, Ahora!",
 			"descripcion":"Nuestro programa de apoyo, nivelación y refuerzo para Bachillerato busca lograr un aprendizaje completo y eficaz en las materias impartidas, de manera integral para cada alumno.",
-			"image": "quinto-slider.webp",
-			"image_2": "sexto-slider.webp",
+			"image": "bachillerato-estudiando.webp",
+			"image_2": "bachillerato-laboratorio.webp",
 			"elemento1": "elemento-molecular.svg",
 			"elemento2": "elemento-poligonal.svg",
 			"elemento3": "elemento-sol.svg",
 			"elemento4": "flecha-abajo-izquierda.svg",
 			"categoria": "bachillerato",
-			"detalle_1": "escolar_1.jpeg",
-			"detalle_2": "escolar_2.jpeg",
-			"detalle_3": "escolar_3.jpeg"
+			"detalle_1": "bachillerato-1.webp",
+			"detalle_2": "bachillerato-2.webp",
+			"detalle_3": "bachillerato-3.webp"
 		} 
 	]
 }
